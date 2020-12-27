1ffbb87d9c820e2c03f308c03239baa402fbf34f
jafar95 <jafar.sadik@northsouth.edu>
2020-12-28 01:28:12 +0600

    another

---
+++ MathsAndStatistics/createPercentileTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE percentile_test (numbers integer);
+
+INSERT INTO percentile_test (numbers)
+VALUES (1),
+    (2),
+    (3),
+    (4),
+    (5),
+    (6);
---
+++ MathsAndStatistics/sumAndAvg.sql
@@ -0,0 +1,3 @@
+SELECT SUM(p0010001) AS "County sum",
+    ROUND(AVG(p0010001)) AS "County avg"
+FROM us_countries_2010;
